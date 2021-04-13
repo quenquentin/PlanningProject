c85c39b27a1467ab7c3ba97e054a1b9c4e5350cc
QuenQuentin <cdql@hotmail.fr>
2021-04-13 13:17:58 +0200

    Ajout des fichiers

---
+++ code.js
@@ -0,0 +1,5 @@
+/* Ce programme est destiné à la création automatique d'un planning selon
+certains critères prédéfinis et modifiables. */
+
+let test = Math.floor(Math.random() * 5+1);
+console.log(test);
